4525474a294b0786aeee881f7b3fd0e5bf2cd3e8
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2020-10-26 15:40:02 +0100

    init: Init files

---
+++ README.md
@@ -0,0 +1,18 @@
+# Kinomap Test
+
+This project is a test from Kinomap
+
+## Install
+
+In order to install this project you need to 
+
+`
+    npm install
+`
+
+Then for runnning this you'll have to :
+
+`
+    npm run build:sass
+    npm start
+`
---
+++ index.js
@@ -0,0 +1,29 @@
+// Import Express Module
+const express = require('express');
+const app = express();
+const controller = require('./src/controllers/indexController');
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Pug as our template engine
+app.set('view engine', 'pug');
+
+// serve static files from the `public` folder
+app.use(express.static(__dirname + '/public'));
+
+// serve assets from node_modules
+app.use('/assets', express.static(__dirname + '/node_modules/'));
+
+// Set the server listening on port 7000
+const server = app.listen(7000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
+
+// Routing / to default.pug and set the title to Accueil
+app.get('/', (req, res) => {
+    res.render('default', {
+        title:  'Carousel',
+        description: 'Bienvenue sur le test Kinomap de Benjamin Catouillard',
+    });
+});
